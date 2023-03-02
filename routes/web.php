27aed07868b9f32d420ad4968f2582121c75e1e8
Nestel23 <n.nestel@hotmail.com>
2023-03-02 08:11:04 -0500

    prueba

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('/', function () {
     return view('inicio');
 });
 
-Route::resource('propietarios', App\Http\Controllers\PropietariosController::class);
-Route::resource('mascotas', App\Http\Controllers\MascotasController::class);
+// Route::resource('propietarios', App\Http\Controllers\PropietariosController::class);
+// Route::resource('mascotas', App\Http\Controllers\MascotasController::class);

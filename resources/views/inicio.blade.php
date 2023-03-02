53871dc3bba3d42d49ceed2641e91a2238f93c76
Nestel23 <n.nestel@hotmail.com>
2023-03-02 12:32:36 -0500

    Update inicio.blade.php

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -6,7 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Veterinaria</title>
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{asset('css/all.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://veterinaria-curso-production.up.railway.app/css/all.min.css" type="text/css">
+
+    
 </head>
 <body>
 
@@ -42,4 +46,5 @@
 </body>
 
 <script type="text/javascript" src="{{asset('js/bootstrap.bundle.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>

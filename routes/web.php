070ec870ade01f784bd7847d753b74306b54ff1d
Nestel23 <n.nestel@hotmail.com>
2023-03-02 08:14:24 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('inicio');
+    return view('welcome');
 });
 
 // Route::resource('propietarios', App\Http\Controllers\PropietariosController::class);
